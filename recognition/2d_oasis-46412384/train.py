f91a8143c3af74f5c3c116f6e5304d47b0f04788
s4684 <s4684@users.noreply.github.com>
2025-11-04 03:28:51 +1000

    Implemented simple status updates and model saving for training module

--- recognition/2d_oasis-46412384/train.py
+++ recognition/2d_oasis-46412384/train.py
@@ -6,16 +6,20 @@ from modules import UNet, MCDiceLoss
 
 
 class Config:
-    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     DATA_DIR = "data/"
-    SUBSET_SIZE = -1
+    MODEL_SAVE_FILE = "_tdmodel"
+    LOSS_SAVE_FILE = "_tdmodel-loss"
+
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+    SUBSET_SIZE = 100
 
     LEARN_RATE = 1e-3
 
     IN_CHANNELS = 1
     NUM_CLASSES = 4
     BATCH_SIZE = 4
-    NUM_EPOCHS = 20
+    NUM_EPOCHS = 3
     VISUALISE_EVERY = 5
 
 
@@ -25,7 +29,6 @@ def train_epoch(model, dl, optim, crit, num_classes, dev="cpu"):
     model.train()
     
     for bat_idx, (imgs, msks) in enumerate(dl):
-        print("\tBATCH: %d" % bat_idx)
         imgs = imgs.to(dev)
         msks = msks.to(dev)
 
@@ -60,32 +63,50 @@ def evaluate(model, dl, crit, dev="cpu"):
     return loss / len(dl)
 
 
-def train(model, dl_train, dl_test, dl_validate, epochs=20, visualise_every=10):
+def train(model, dl_train, dl_test, epochs=20, visualise_every=10):
     model.to(Config.DEVICE)
+    print("Starting model training on %s" % Config.DEVICE)
 
     crit = MCDiceLoss()
     # Use AdaM optimiser
     optim = Adam(model.parameters(), lr=Config.LEARN_RATE)
 
     train_losses = []
-
     test_losses = []
 
     for ep_idx in range(epochs):
-        train_losses.append(train_epoch(
+        print("\t[ Epoch %d / %d ]" % (ep_idx + 1, epochs), end="", flush=True)
+        
+        train_loss = train_epoch(
             model,
             dl_train,
             optim,
             crit,
             Config.NUM_CLASSES,
             Config.DEVICE
-        ))
+        )
 
-        test_losses.append(evaluate(
+        test_loss =evaluate(
             model, 
             dl_test,
             crit,
             Config.DEVICE
-        ))
+        )
+
+        print("\tTrain: %6.5f\tTest: %6.5f" % (train_loss, test_loss))
+
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+
+    print("Training complete!")
 
     return train_losses, test_losses
+
+
+if (__name__ == "__main__"):
+    dl_train, dl_test, _ = get_oasis_dataloaders(Config.DATA_DIR, Config.BATCH_SIZE, Config.SUBSET_SIZE)
+    model = UNet(Config.IN_CHANNELS, Config.NUM_CLASSES)
+    train_loss, test_loss = train(model, dl_train, dl_test, epochs=Config.NUM_EPOCHS)
+
+    torch.save(model.state_dict(), Config.MODEL_SAVE_FILE)
+    torch.save([train_loss, test_loss, []], Config.LOSS_SAVE_FILE)
